90d87c10f78650730a8ea9eb0acac5ac16b5f6bc
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-06-05 21:11:54 +0000

    Revert "fix py req"
    This reverts commit 9fdaf6f91218a38f76f91a7aae77ce9734eabfe7.

--- Shared/setup.py
+++ Shared/setup.py
@@ -25,11 +25,11 @@ setup(
     author="The Regents of the University of California",
     author_email="FIXME",
     packages=find_packages(),
-    python_requires=">=3.6, <3.8", ## FIXME: require 3.7, maybe more, after dependencies upgrades
+    python_requires=">=3.5, <3.6", ## FIXME: require 3.7, maybe more, after dependencies upgrades
     install_requires=[  ##
         ## FIXME: can we ease up on the version requirements?
         ## Maybe, but it's nice for intra-workspace consistency. 
-        "numpy==1.21.2",
+        "numpy==1.16.4",
     ],
     # extras_require={
     #     "dev": [
